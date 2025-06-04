1efded58bc95ba8bbc6e52b34df8719be0e7b2ca
Kristoffer Lorentsen <krilor@gmail.com>
2025-06-04 20:49:49 +0200

    feat: Add HTMX and live reload functionality

--- main.go
+++ main.go
@@ -10,16 +10,33 @@ const htmlContent = `
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple Go Web Page</title>
-    <style>
-        body { font-family: sans-serif; display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0; background-color: #f0f0f0; }
-        h1 { color: #333; }
-    </style>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Simple Go Web Page</title>
+<style>
+body { font-family: sans-serif; display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0; background-color: #f0f0f0; }
+h1 { color: #333; }
+</style>
+<script>
+	conn = new WebSocket("ws://" + document.location.host + "/devlivereload");
+	conn.onclose = function (evt) {
+	console.log("Connection Closed")
+	setTimeout(function () {
+		location.reload();
+	}, 2000);
+	};
+</script>
+<script src="https://unpkg.com/htmx.org@2.0.4" integrity="sha384-HGfztofotfshcF7+8n44JQL2oJmowVChPTg48S+jvZoztPfvwD79OC/LTtG6dMp+" crossorigin="anonymous"></script>
 </head>
 <body>
     <h1>Hello from Go!</h1>
+	<div id="parent-div">Its me</div>
+	<button hx-post="/clicked"
+    hx-trigger="click"
+    hx-target="#parent-div"
+    hx-swap="innerHTML">
+    Click Me!
+</button>
 </body>
 </html>
 `
@@ -31,6 +48,10 @@ func serveHTML(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", serveHTML)
+	http.Handle("/clicked", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "You clicked the button!")
+	}))
+
 	log.Println("Starting server on :8080...")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
